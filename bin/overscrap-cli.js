c69058265a5132a535ea561c31795825b762a840
hoshin <martin.bahier@gmail.com>
2017-08-27 18:23:00 +0200

    Cleanup linting issues in bin/cli tool

--- bin/overscrap-cli.js
+++ bin/overscrap-cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-var OverScrap = require('../dist/overScrap.min');
+const OverScrap = require('../dist/overScrap.min');
 
 if(process.argv.length < 3){
   console.error('Not enough arguments')
@@ -8,7 +8,7 @@ if(process.argv.length < 3){
   process.exit(1);
 }
 
-var overScrap = new OverScrap();
+const overScrap = new OverScrap();
 console.log(`Looking up data for ${process.argv[2]} on realm '${process.argv[3] || 'default'}' for the '${process.argv[4] || 'default'}' mode `)
 overScrap.loadDataFromProfile(process.argv[2], process.argv[3], process.argv[4]).then(data => {
   console.log(JSON.stringify(data, null, 2));
@@ -20,4 +20,4 @@ overScrap.loadDataFromProfile(process.argv[2], process.argv[3], process.argv[4])
     console.log('Could not find the page, there\'s probably an issue with the Battle tag, region or game modes provided. Please double check them, retry, and submit a report if it still fails =)' )
   }
   process.exit(2)
-})
+});
